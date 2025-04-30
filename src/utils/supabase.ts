046ed7cfa75cec3a190309e8f30d085eae4aa6aa
ぱもいー <molpeko02@gmail.com>
2025-04-30 23:01:09 +0900

    fix:環境変数の差印象方法を編集

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,7 +1,8 @@
-// lib/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export { supabase };
